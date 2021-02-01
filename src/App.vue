a763713898b0e3212bd0415f43493eebe2991e58
xingchang <>
2021-02-01 21:41:47 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <template>
   <div id="app">
-    <!-- 连接 -->
+    <!-- 登录 -->
     <el-row class="row">
+      <el-col :span="2" class="col">
+        <el-input v-model="user.userID" :disabled="user.signed" placeholder="请输入UserID"></el-input>
+      </el-col>
+      <el-col :span="2" class="col">
+        <el-input v-model="user.vcode" disabled placeholder="请输入验证码"></el-input>
+      </el-col>
+      <el-col :span="1" v-if="user.vcode.length===0">
+        <el-button type="primary" @click="doGetVCode">获取验证码</el-button>
+      </el-col>
+      <el-col :span="1" v-else>
+        <el-button type="primary" @click="doUserLogin">登录</el-button>
+      </el-col>
+    </el-row>
+    <!-- 连接 -->
+    <el-row class="row" v-if="user.token.length===36">
       <el-col :span="3" class="col">
-        <el-select v-model="wsUrl" placeholder="请选择网关">
+        <el-select v-model="host" :disabled="isConnected" placeholder="请选择网关">
           <el-option
             v-for="item in gatewayList"
             :key="item.id"
@@ -17,12 +32,6 @@
         <el-button type="primary" :loading="loading" @click="doConnect">{{isConnected ? '断开连接' : '连接'}}</el-button>
       </el-col>
     </el-row>
-    <!-- 输入 -->
-    <el-row class="row" v-if="isConnected">
-      <hr />
-      <el-col :span="4" class="col">
-      </el-col>
-    </el-row>
     <!-- 发送按钮 -->
     <el-row class="row" v-if="isConnected">
     </el-row>
@@ -33,41 +42,31 @@
 </template>
 
 <script>
-import pb from '@/pb/00_package_pb.js'
+import Engine from '@/engine/engine.js'
 import axios from 'axios'
+
+var userInfo = {
+  userID: '10001',
+  token: '',
+  macAddr: 'AABBCCEEFF00',
+  deviceCode: 'windows10',
+  signed: false,
+  vcode: ''
+}
 
 export default {
   name: 'App',
   data () {
     return {
       gatewayList: [],
+      engine: null,
+      user: userInfo,
+      loading: false,
       isConnected: false,
-      wsUrl: '',
-      loading: false
+      host: ''
     }
   },
-  created () {
-    var this_ = this
-    axios.get('http://127.0.0.1/cgi/get_ws_gateway_list').then(resp => {
-      if (resp.status === 200 && resp.data.code === 0) {
-        console.log(resp.data.data)
-        this_.gatewayList = resp.data.data
-      }
-    }).catch(err => {
-      console.log(err)
-    })
-  },
   methods: {
-    rowColor ({ row, rowIndex }) {
-      if (this.messages[rowIndex].direction % 2 === 0) {
-        return 'recv-row'
-      } else {
-        return 'send-row'
-      }
-    },
-    doClear () {
-      this.messages = []
-    },
     doConnect () {
       if (this.isConnected) {
         this.disconnect()
@@ -76,130 +75,118 @@ export default {
       }
     },
     disconnect () {
-      this.ws.close()
+      this.engine.disconnect()
     },
     connect () {
       var this_ = this
-      var ws = new WebSocket(`ws://${this.wsUrl.trim()}`)
-      ws.binaryType = 'arraybuffer'
-
-      ws.onerror = function (e, err) {
-        this_.$message({
-          message: `连接 ${this_.wsUrl} 失败`,
-          type: 'error'
-        })
-      }
-
-      ws.onopen = function () {
-        this_.isConnected = true
-        this_.$message({
-          message: `连接 ${this_.wsUrl} 成功`,
-          type: 'success'
-        })
-      }
-
-      ws.onclose = function (ev) {
-        if (!this_.isConnected) {
-          return
-        }
-        this_.isConnected = false
-        this_.messages = []
-        this_.loading = false
-        this_.$message({
-          message: `断开连接 ${this_.wsUrl} 成功`,
-          type: 'success'
-        })
-      }
-
-      ws.onmessage = function (ev) {
-        var msg = pb.Message.deserializeBinary(ev.data)
-        var body = msg.getBody()
-        var router = msg.getRouter()
-        var txt = ''
-        if (body.getData().length > 0) {
-          txt = new TextDecoder('utf-8').decode(body.getData())
-        }
-        if (body.getMid() < pb.MessageID.OK) {
+      this_.engine = new Engine({
+        host: this_.host,
+        router: '/',
+        openHandler: function (ev) {
           this_.$message({
-            message: `错误: ${body.getMid()}`,
+            message: '连接成功',
+            type: 'success'
+          })
+          this_.isConnected = true
+        },
+        errorHandler: function (ev, err) {
+          this_.$message({
+            message: err.message,
             type: 'error'
           })
-          this_.loading = false
-          return
-        }
-        this_.messages.unshift({
-          direction: 2,
-          time: this_.$moment(new Date().getTime()).format('YYYY-MM-DD HH:mm:ss.SSS'),
-          mid: body.getMid(),
-          data: txt,
-          idempotent: body.getIdempotent(),
-          checkCode: body.getCheckcode(),
-          mount: router.getMount(),
-          from: router.getFrom(),
-          to: router.getToList()[0]
-        })
-        if (this_.batch) {
-          if (--this_.batchNum === 0) {
-            this_.loading = false
-          }
-        } else {
-          this_.loading = false
-        }
-      }
-      this.ws = ws
-    },
-    doSend () {
-      var this_ = this
-
-      // message body
-      var data = new TextEncoder().encode(this_.msg.data)
-      var body = new pb.MessageBody()
-      body.setMid(this_.msg.mid)
-      body.setData(data)
-      body.setCheckcode(data[0] ^ data[data.length - 1])
-      body.setIdempotent(new Date().getTime())
-
-      // router
-      var router = new pb.Router()
-      router.setToList([this_.msg.to])
-
-      // message
-      var msg = new pb.Message()
-      msg.setBody(body)
-      msg.setRouter(router)
-      var rbuf = msg.serializeBinary()
-
-      if (this_.batch) {
-        for (let i = 0; i < this_.batchNum; i++) {
-          this_.loading = true
-          this_.ws.send(rbuf)
-          this_.messages.unshift({
-            direction: 1,
-            time: this_.$moment(new Date().getTime()).format('YYYY-MM-DD HH:mm:ss.SSS'),
-            mid: body.getMid(),
-            data: this_.msg.data,
-            idempotent: body.getIdempotent(),
-            checkCode: body.getCheckcode(),
-            mount: '',
-            from: '',
-            to: router.getToList()[0]
+        },
+        closeHandler: function (ev) {
+          this_.$message({
+            message: '连接已断开',
+            type: 'warning'
           })
+          this_.isConnected = false
         }
-      } else {
-        this_.loading = true
-        this_.ws.send(rbuf)
-        this_.messages.unshift({
-          direction: 1,
-          time: this_.$moment(new Date().getTime()).format('YYYY-MM-DD HH:mm:ss.SSS'),
-          mid: body.getMid(),
-          data: this_.msg.data,
-          idempotent: body.getIdempotent(),
-          checkCode: body.getCheckcode(),
-          mount: '',
-          from: '',
-          to: router.getToList()[0]
+      })
+      this_.engine.connect(this_.user).then(engine => {
+        this_.$message({
+          message: '连接网关成功',
+          type: 'success'
         })
-      }
+      }).catch(err => {
+        this_.$message({
+          message: err.message,
+          type: 'error'
+        })
+      })
+    },
+    doGetVCode () {
+      var this_ = this
+      axios.post('http://127.0.0.1/cgi/get_validate_code', {
+        'phoneNum': this_.user.userID
+      }).then(rsp => {
+        if (rsp.status !== 200) {
+          return Promise.reject(new Error('请求服务异常'))
+        }
+
+        if (rsp.data.code !== 0) {
+          return Promise.reject(new Error(rsp.data.error))
+        }
+
+        this_.user.vcode = rsp.data.data
+        this_.$message({
+          message: '获取验证码成功',
+          type: 'success'
+        })
+      }).catch(err => {
+        this_.$message({
+          message: err.message,
+          type: 'error'
+        })
+      })
+    },
+    doUserLogin () {
+      var this_ = this
+      axios.post('http://127.0.0.1/cgi/user_login', {
+        'phoneNum': this_.user.userID,
+        'vcode': this_.user.vcode,
+        'macAddr': this_.user.macAddr,
+        'deviceCode': this_.user.deviceCode
+      }).then(ret => {
+        if (ret.status !== 200) {
+          return Promise.reject(new Error('网络异常, 请稍后再试'))
+        }
+
+        if (ret.data === undefined || ret.data === null) {
+          return Promise.reject(new Error('请求服务异常, 请稍后再试'))
+        }
+
+        return Promise.resolve(ret.data)
+      }).then(rsp => {
+        if (rsp.code !== 0) {
+          return Promise.reject(new Error(rsp.error))
+        }
+
+        this_.user.token = rsp.data.token
+        this_.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+
+        return Promise.resolve('http://127.0.0.1/cgi/get_ws_gateway_list')
+      }).then(url => {
+        var this_ = this
+        axios.get(url).then(resp => {
+          if (resp.status === 200 && resp.data.code === 0) {
+            this_.gatewayList = resp.data.data
+          }
+        }).catch(err => {
+          this_.$message({
+            message: err.message,
+            type: 'error'
+          })
+        })
+      }).catch(err => {
+        this_.$message({
+          message: err.message,
+          type: 'error'
+        })
+      })
     }
   }
 }
---
+++ src/engine/engine.js
@@ -0,0 +1,88 @@
+import pb from '@/pb/00_package_pb.js'
+
+export default class Engine {
+  constructor ({host, router, openHandler, errorHandler, closeHandler}) {
+    this.host = host
+    this.router = router
+    this.ws = null
+    this.connected = false
+    this.closeHandler = closeHandler
+    this.errorHandler = errorHandler
+    this.openHandler = openHandler
+  }
+
+  disconnect () {
+    if (this.ws !== null && this.connected) {
+      this.ws.close()
+    }
+  }
+
+  connect (user) {
+    var err = ''
+    var this_ = this
+    do {
+      this_.ws = new WebSocket(`ws://${this_.host}${this_.router}`)
+
+      if (this_.ws === undefined || this_.ws === null) {
+        err = `connect ws://${this_.host}${this_.router} failed`
+        break
+      }
+
+      this_.ws.onopen = (ev) => {
+        this_.openHandler()
+        this_.connected = true
+        this_.userSignIn(user)
+      }
+
+      this_.ws.onclose = (ev) => {
+        this_.closeHandler(ev)
+        this_.connected = false
+      }
+
+      this_.ws.onerror = (ev, err) => {
+        this_.errorHandler(ev, err)
+      }
+
+      this_.ws.onmessage = this_.onMessage
+      this_.ws.binaryType = 'arraybuffer'
+    } while (0)
+
+    if (err.length > 0) {
+      return Promise.reject(new Error(err))
+    }
+
+    return Promise.resolve(this_)
+  }
+
+  userSignIn ({userID, token, macAddr, deviceCode}) {
+    var req = new pb.UserSignInReq()
+    req.setUserid(userID)
+    req.setToken(token)
+    req.setMacaddr(macAddr)
+    req.setDevicecode(deviceCode)
+
+    var data = req.serializeBinary()
+    var pack = new pb.Package()
+    pack.setPid(pb.PackageID.PID_USERSIGNINREQ)
+    pack.setData(data)
+    pack.setCheckcode(data[0] ^ data[data.length - 1])
+    pack.setIdempotent(new Date().getTime())
+    console.log(pack.toObject())
+    var pbuff = pack.serializeBinary()
+    this.ws.send(pbuff)
+  }
+
+  onError (ev, err) {
+    this.errorHandler(ev, err)
+  }
+
+  onMessage (ev) {
+    var pack = pb.Package.deserializeBinary(ev.data)
+    console.log(pack.toObject())
+    switch (pack.getPid()) {
+      case pb.PackageID.PID_USERSIGNINRSP:
+        var rsp = pb.UserSignInRsp.deserializeBinary(pack.getData())
+        console.log(rsp.toObject())
+    }
+  }
+}
