f28a7f0cd74c980e61ece5f3c864cb9cd3e44933
xingchang <>
2021-03-12 09:23:53 +0700

    update

--- src/views/Live.vue
+++ src/views/Live.vue
@@ -6,9 +6,23 @@
 
 <script>
 import flvjs from 'flv.js'
+import {encrypt, decrypt, PrivateKey} from 'eciesjs'
 
 export default {
   name: 'Live',
+
+  created () {
+    const data = Buffer.from('hello world!!!')
+
+    const k1 = PrivateKey.fromHex('99095d3fe23093e2a013a6ae65d6969e3993cf48444120fef1ac798ce3016045')
+    console.log(k1.toHex())
+    console.log(k1.publicKey.toHex())
+
+    var str = encrypt(k1.publicKey.toHex(), data)
+    console.log(str.toString('hex'))
+    var dst = decrypt(k1.toHex(), str)
+    console.log(dst.toString())
+  },
 
   data () {
     return {
@@ -52,7 +66,4 @@ export default {
 </script>
 
 <style scoped>
-.vjs-tech {
-  /* pointer-events: none; */
-}
 </style>
